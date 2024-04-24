2e73f2b090579a77cb2d737cd09db023f4f8f2e9
Tim <tim300cyc@gmail.com>
2024-04-24 11:32:14 +0800

    Initial Commit

---
+++ GitHub Exercise/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  GitHub Exercise
+//
+//  Created by PrickupDriver on 2024/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+                
+            Text("Version 1.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
+        }
+        .padding()
+        .foregroundStyle(.red)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by PrickupDriver on 2024/4/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
